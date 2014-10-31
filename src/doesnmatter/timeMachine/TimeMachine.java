1743eb2ffeef93f2e16a3c5cab8380e25f307b6b
CirahYan <cirahnitex@gmail.com>
2014-10-31 15:29:50 +0800

    implement TimeMachine

--- src/doesnmatter/timeMachine/TimeMachine.java
+++ src/doesnmatter/timeMachine/TimeMachine.java
@@ -5,12 +5,12 @@ import java.util.Date;
 //set server time and return time
 public class TimeMachine
 {
-
-	private static long serverTime;
+	private static long timeDiff = 0L;
 	public static Date getNow() {
-		return new Date();
+		if(timeDiff==0) return new Date();
+		return new Date(new Date().getTime() + timeDiff);
 	}
-	public static void setNow(long timeStamp) {
-		serverTime = timeStamp;
+	public static void setNow(long timestamp) {
+		timeDiff = timestamp - new Date().getTime();
 	}
 }
