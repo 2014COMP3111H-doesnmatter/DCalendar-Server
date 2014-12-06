7a98d12bd40020307645da82a412ff0c32db3684
CirahYan <cirahnitex@gmail.com>
2014-12-06 23:58:42 +0800

    just a debug file

---
+++ src/api/debug.java
@@ -0,0 +1,30 @@
+package api;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Map;
+
+import org.json.JSONObject;
+
+import db.Appointment;
+import db.Venue;
+import doesnserver.Session;
+
+
+public class debug extends ApiHandler
+{
+
+	public debug()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public JSONObject main(Map<String, String> params, Session session)
+			throws Exception {
+		JSONObject rtn = new JSONObject();
+		rtn.put("rtnCode", this.getRtnCode(200));
+		rtn.put("value", "a");
+		return rtn;
+	}
+
+}
